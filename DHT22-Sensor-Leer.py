654b6dc8cc261e278bf9b8c3937a06dba8f3afd8
NiPeGun <nipegun@gmail.com>
2021-12-24 13:19:18 +0100

    Update DHT22-Sensor-Leer.py

--- DHT22-Sensor-Leer.py
+++ DHT22-Sensor-Leer.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#-----------------------------------------------
+#  Script de NiPeGun para leer el sensor DHT22
+#-----------------------------------------------
+
 import Adafruit_DHT
 import json
 
